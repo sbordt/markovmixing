bc62533c80a30cb48b0927b246ff183cb76db6c7
Sebastian Bordt <sbordt@posteo.de>
2015-10-18 18:38:37 +0200

    Fixing travis build.

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ setup(
 	description = 'Determine mixing times for Markov chains with ~ 50.000 states.',
 	license = 'MIT',
 	url = 'https://github.com/sbordt/markovmixing',
-    install_requires=[
-    "networkx >= 1.9",
-    "numpy >= 1.8.2",
-    "scipy >= 0.13.3",
-    "matplotlib >= 1.3.1",
-    "nose >= 1.3.1",
+	install_requires=[
+	"networkx >= 1.9",
+	"numpy >= 1.8.2",
+	"scipy >= 0.13.3",
+	"matplotlib >= 1.3.1",
+	"nose >= 1.3.1",
 ],
 )
